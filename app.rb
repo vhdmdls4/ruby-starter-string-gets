af03a6af7a06b9f324bfd2bbba8c7a43bc512901
Victor Hugo <victor.hugo@omnilogic.ai>
2023-12-27 21:55:12 -0300

    start: adding first local files to repo

---
+++ app.rb
@@ -0,0 +1,38 @@
+def hello
+  puts "Hi"
+end
+
+
+
+=begin
+String Manipulation Tool
+Features:
+  String Reversal: Reverses a given string.
+  String Uppercase/Lowercase: Converts a given string to uppercase or lowercase.
+  Substring Finder: Finds a substring within a given string.
+  String Concatenation: Concatenates two strings together.
+  String Length: Calculates the length of a given string.
+  Important Concepts:
+              Functions/Methods: Create separate methods for each feature to encapsulate functionality.
+  User Input Handling: Use gets.chomp to receive user input from the command line.
+  String Manipulation: Utilize built-in Ruby methods like reverse, upcase, downcase, include?, concat, length, etc.
+=end
+
+def main
+
+  puts "Welcome to my first App using Ruby"
+  puts "Here, you can manipulate strings in certain ways"
+  puts "Here the current methods available: "
+
+
+end
+
+main if __FILE__ == $PROGRAM_NAME
+
+=begin
+How This Helps:
+Understanding Ruby Methods: Creating methods for each feature enhances your grasp of defining and using functions in Ruby.
+Handling User Input: Utilizing gets.chomp for user input familiarizes you with input/output operations in Ruby.
+String Manipulation Techniques: Implementing various string manipulation operations strengthens your understanding of string methods available in Ruby.
+Feel free to expand upon this skeleton code, add more features, error handling, or any other enhancements to deepen your understanding of Ruby programming.
+=end
